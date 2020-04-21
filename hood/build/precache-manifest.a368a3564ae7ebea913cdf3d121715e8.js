fae488ff4d12f97c464a6d14f28e379ef33fef31
Abhishek Hedaoo <abhihedaoo98@gmail.com>
2020-04-21 10:10:15 +0530

    production files

---
+++ hood/build/precache-manifest.a368a3564ae7ebea913cdf3d121715e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407ec1c4cf2d00966bd58502b9a89073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7e1270998616cc1004",
+    "url": "/static/css/main.f86e9441.chunk.css"
+  },
+  {
+    "revision": "479df17cf746603c7491",
+    "url": "/static/js/2.d6f65134.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6f65134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7e1270998616cc1004",
+    "url": "/static/js/main.a1ae7851.chunk.js"
+  },
+  {
+    "revision": "215e07b5bab75a24ab02",
+    "url": "/static/js/runtime-main.27cccc14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
